0bf85b36f4c9d58261566887c174cb87ec30b717
Mahmoud Tarek <mtarek@spark-sys.com>
2024-01-22 23:29:34 +0300

    added sql file

--- Efcore-vs-Raw-join-performance/Efcore-vs-Raw-join-performance/obj/Debug/net7.0/Efcore-vs-Raw-join-performance.AssemblyInfo.cs
+++ Efcore-vs-Raw-join-performance/Efcore-vs-Raw-join-performance/obj/Debug/net7.0/Efcore-vs-Raw-join-performance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Efcore-vs-Raw-join-performance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09744fb13c363abb4d1636116b69290c71acd7f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Efcore-vs-Raw-join-performance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Efcore-vs-Raw-join-performance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
